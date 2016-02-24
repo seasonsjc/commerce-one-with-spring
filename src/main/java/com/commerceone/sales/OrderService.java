e2c9bc67ad934b14e5b07ee22f7a60377f37772a
Eduardo R Carvalho - bisso <ercarval@gmail.com>
2016-02-24 20:59:28 -0300

    start de transacoes

--- src/main/java/com/commerceone/sales/OrderService.java
+++ src/main/java/com/commerceone/sales/OrderService.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService {
@@ -20,6 +21,20 @@ public class OrderService {
 	public OrderService() {			
 		logger.debug( ".... Created OrderService and repository is " +  repository );
 	}
+	
+	
+	@Transactional
+	public void createSalesOrder (Order order) {
+		
+		repository.create(order);
+		billingService.startBillingFor(order);
+		notification.sendMailToSupplier(order);
+		notification.sendMailToCustomer(order);
+		integrator.sendToSupplier(order);
+		integrator.sendToCustomer(order);
+			
+	}
+	
 	
 	@PostConstruct
 	public void init() {
